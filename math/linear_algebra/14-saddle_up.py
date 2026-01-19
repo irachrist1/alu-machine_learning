179a3412c13fd277453c09be025374ff8b5142d5
irachrist1 <spcsgaba@gmail.com>
2026-01-19 07:18:19 +0200

    Add saddle up exercise

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""NumPy matrix multiplication helper."""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Return the matrix product of two arrays."""
+    return np.matmul(mat1, mat2)
